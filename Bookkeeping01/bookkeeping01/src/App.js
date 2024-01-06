c5b5477fc471ff99ecb97245f6b3ac58df8f3f7c
Navodh1001 <navodh.belle@gmail.com>
2024-01-07 02:39:18 +0530

    Bookkeep Page 01
    Frontend Code - Bookkeeper page 01 - 03

---
+++ Bookkeeping01/bookkeeping01/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import Book from './Components/Book'
+
+
+
+function App() {
+  return (
+    <div>
+     <Book/>
+    </div>
+  );
+}
+
+export default App;
